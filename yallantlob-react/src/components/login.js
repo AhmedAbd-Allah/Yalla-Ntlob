179447f4b21e67237a06d9e0b0893816c9c5d26a
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2018-04-09 22:51:00 +0200

    Please enter the commit message for your changes. Lines starting

--- yallantlob-react/src/components/login.js
+++ yallantlob-react/src/components/login.js
@@ -1,12 +1,79 @@
 import '../index.css';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Icon, Input, Header, Image, Form, Label, Button, Grid, Segment } from 'semantic-ui-react';
 import FacebookLoginButton from 'react-social-login-buttons/lib/buttons/FacebookLoginButton';
 import GoogleLoginButton from 'react-social-login-buttons/lib/buttons/GoogleLoginButton';
+import axios from 'axios';
+
 
 
 class Login extends Component {
+    constructor(props) 
+        {
+                super(props);
+                this.state = {"email":'', "password":''};
+                this.setEmail = this.setEmail.bind(this);
+                this.setPassword = this.setPassword.bind(this);
+                this.loginFunction = this.loginFunction.bind(this);   
+        }
+    setEmail(e)
+    {
+            this.setState({"email": e.target.value});
+    }
+
+    setPassword(e)
+    {
+            this.setState({"password": e.target.value});
+    }
+    loginFunction(e)
+    {
+        e.preventDefault();
+            const data = {"auth":this.state}
+            console.log( data)
+    axios.post('http://localhost:3000/user_token', data
+              ).then(function (response) {
+            console.log(response);
+            console.log(response.data.jwt);
+            console.log(response.status)
+            if (response.status == 201)
+            {
+                localStorage.setItem('token',response.data.jwt)
+                axios.get('http://localhost:3000/auth', 
+                {headers:{
+                            'Content-Type': 'application/json',
+                            'Authorization':"Bearer "+localStorage.getItem('token')
+                         }
+                        }).then(function (response) {
+                            console.log(response);
+                            console.log(response.data.msg);
+                            console.log(localStorage.getItem('token'));
+                            localStorage.setItem('user',JSON.stringify(response.data.msg))
+                            var u=localStorage.getItem('user')
+                            console.log('User from local storage',JSON.parse(u));
+                            // return <Redirect to={'/HomePage'}/>
+                        //   return  < Redirect to={{ pathname: "/HomePage"}} />
+                          <Link to="/HomePage">Protected Page</Link>
+                         })
+                        .catch(function (error) {
+                            console.log(error);
+                            
+                        });
+
+            }
+            // return <Redirect to="/HomePage"/>
+          })
+          .catch(function (error) {
+            // console.log(Request.status);
+            
+          });
+    }
+
+
+
+
+
+
     render(){
         return (
             <div>
@@ -24,18 +91,26 @@ class Login extends Component {
                                         action={{ color: 'teal', labelPosition: 'left', icon: 'at', content: 'Email' }}
                                         actionPosition='left'
                                         placeholder='Email'
+                                        required
+                                        type="email"
+                                        value={this.state.email}
+                                        onChange={this.setEmail}
                                     />
                                     <br />
                                     
                                     <Form.Input
                                         action={{ color: 'teal', labelPosition: 'left', icon: 'lock', content: 'Password' }}
                                         actionPosition='left'
-                                        placeholder='Password'   
+                                        placeholder='Password'
+                                        type="password"
+                                        required
+                                        value={this.state.password}
+                                        onChange={this.setPassword}    
                                     />
                                     <br />
-                                    <Link to="/HomePage">
-                                    <Button secondary size='large'>Login</Button>
-                                    </Link>
+                                    
+                                    <Button secondary size='large' onClick={this.loginFunction}>Login</Button>
+                                    
                               
                             </Segment>
                         </Form>
--- yallantlob-react/src/components/register.js
+++ yallantlob-react/src/components/register.js
@@ -1,20 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import React, { Component }from 'react';
 import ReactUploadFile from 'react-upload-file';
-import { Icon, Input, Header, Image, Form, Label, Button, Grid, Segment } from 'semantic-ui-react';
+import { Icon, Input, Header, Image, Form, Label, Button, Grid, Segment, Message } from 'semantic-ui-react';
 import axios from 'axios';
-
-
-
-
+import FileBase64 from 'react-file-base64';
+// import base64Img from 'base64-img';
 
 class Register extends Component {
 
         constructor(props) 
         {
                 super(props);
-                this.state = {name: '',email:'', password:'', confpassword:'',image:''};
-                           
+                this.state = {name: '',email:'', password:'', confpassword:'',image:'',errors:'',redirect: false};                   
                 this.setName = this.setName.bind(this);
                 this.setEmail = this.setEmail.bind(this);
                 this.setPassword = this.setPassword.bind(this);
@@ -25,49 +22,75 @@ class Register extends Component {
 
         setName(e) 
         {
-                this.setState({ name: e.target.value});
+                this.setState({ "name": e.target.value});
         }
 
         setEmail(e)
         {
-                this.setState({email: e.target.value});
+                this.setState({"email": e.target.value});
         }
 
         setPassword(e)
         {
-                this.setState({password: e.target.value});
+                this.setState({"password": e.target.value});
         }
 
         setconfPassword(e)
         {
-                this.setState({confpassword: e.target.value});
+                this.setState({"confpassword": e.target.value});
         }
 
-        setImage(e)
+        setImage(myimage)
         {
-                this.setState({image: e.target.value});
-        }
-                
+                this.setState({ image: myimage })
+        }       
 
         Submit(e)
         {
+                if (this.state.name.trim()=='')
+                {
+                        this.setState({errors:'Name can not be an empty string'});
+                }
+                
+                else if( this.state.password.trim()=='')
+                {
+                        this.setState({errors:'Password can not be only spaces'});
+                }
+                
+                else if(this.state.password != this.state.confpassword )
+                {
+                        this.setState({errors:'Name is Required'});
+                }
+
+                else
+                {
+                        this.setState({ name: this.state.name.trim()});
+                        this.setState({ email: this.state.email.trim()});
+                        this.setState({ password: this.state.password.trim()});
+                        this.setState({ confpassword: this.state.confpassword.trim()});
+                        // this.setState({ redirect: true});
+
+                        console.log(this.state);
+                        
+                        const data = {"user":this.state};
+                                axios.post('http://localhost:3000/users', 
+                        data).then(function (response) {
+                        console.log(response);
+                      })
+                      .catch(function (error) {
+                        console.log(error);
+                      });
+                }
                
-
-                // console.log(this.state);
-                var data = {"user":this.state}
-                console.log( data)
-
-        axios.post('http://localhost:3000/users', data
-                  ).then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
         }
 
 
         render(){
+                const { redirect } = this.state;
+                if (redirect) 
+                {
+                        return <Redirect to='/login'/>;
+                }
                 return (
                         <div>
                                 <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle' >
@@ -78,8 +101,18 @@ class Register extends Component {
                                                         Yalla Notlob
                                                 </Header>
                                                 <br />
+                                                <label>
+                                                        { this.state.errors !=''?
+                                                        <Message
+                                                        error
+                                                        header=''
+                                                        content={this.state.errors}
+                                                        />
+                                                        :''}
+                                                        </label>
                                                 <Form size='large' onSubmit={this.Submit}>
                                                         <Segment stacked>
+                                                        
                                                                 <Form.Input
                                                                         action={{ color: 'teal', labelPosition: 'left', icon: 'user', content: 'Name' }}
                                                                         actionPosition='left'
@@ -125,19 +158,24 @@ class Register extends Component {
                                                                         value={this.state.confpassword}
                                                                         onChange={this.setconfPassword}   
                                                                 />
-                                                                <br />
                                                                 
-                                                                <Form.Input
+                                                                
+                                                                {/* <Form.Input
                                                                         action={{ color: 'teal', labelPosition: 'left', icon: 'image', content: 'image' }}
                                                                         actionPosition='left'
                                                                         placeholder='No file chosen'
-                                                                        type='file'
+                                                                        // type='file'
                                                                         name='image'
-                                                                        value={this.state.image}
-                                                                        onChange={this.setImage}   
-                                                                />
+                                                                        // value={this.state.image}
+                                                                        // onChange={this.setImage}
+                                                                           
+                                                                > */}
+                                                                <FileBase64
+       multiple={ false }
+       onDone={ this.setImage.bind(this) } />
+       {/* </Form.Input> */}
                                                                 <br />
-                                                                <Button type='submit' secondary size='large'>Register</Button>
+                                                                <Button type='submit' secondary size='large' id ="submitButton">Register</Button>
                                                         </Segment> 
                                                 </Form>           
                                         </Grid.Column>
