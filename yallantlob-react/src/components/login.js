b888ac34b25cd4feb75a93b71c3891e3c0ef91d3
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2018-04-10 20:22:31 +0200

    social media login and usual login

--- yallantlob-react/src/components/login.js
+++ yallantlob-react/src/components/login.js
@@ -1,30 +1,33 @@
 import '../index.css';
 import { Link, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
-import { Icon, Input, Header, Image, Form, Label, Button, Grid, Segment } from 'semantic-ui-react';
+import { Icon, Input, Header, Image, Form, Label, Button, Grid, Segment, Message } from 'semantic-ui-react';
 import FacebookLoginButton from 'react-social-login-buttons/lib/buttons/FacebookLoginButton';
 import GoogleLoginButton from 'react-social-login-buttons/lib/buttons/GoogleLoginButton';
 import axios from 'axios';
+import SocialButton from './SocialButton';
+var randomString = require('random-string')
 
 
-
-class Login extends Component {
+class Login extends Component 
+{
     constructor(props) 
-        {
-                super(props);
-                this.state = {"email":'', "password":'',errors:'',redirect: false};
-                this.setEmail = this.setEmail.bind(this);
-                this.setPassword = this.setPassword.bind(this);
-                this.loginFunction = this.loginFunction.bind(this);   
-        }
+    {
+        super(props);
+        this.state = {"email":'', "password":'',errors:'',redirect: false};
+        this.setEmail = this.setEmail.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+        this.loginFunction = this.loginFunction.bind(this);   
+    }
+
     setEmail(e)
     {
-            this.setState({"email": e.target.value});
+        this.setState({"email": e.target.value});
     }
 
     setPassword(e)
     {
-            this.setState({"password": e.target.value});
+        this.setState({"password": e.target.value});
     }
 
     loginFunction(e)
@@ -34,41 +37,103 @@ class Login extends Component {
         this.setState({ password: this.state.password.trim()});
         const data = {"auth":this.state}
         console.log( data)
-        axios.post('http://localhost:3000/user_token', data
-              ).then(response => {
-            console.log(response);
-            console.log(response.data.jwt);
-            console.log(response.status)
-            if (response.status == 201)
-            {
-                localStorage.setItem('token',response.data.jwt)
-                this.setState({ redirect: true}); 
-                axios.get('http://localhost:3000/auth', 
-                {headers:{
-                            'Content-Type': 'application/json',
-                            'Authorization':"Bearer "+localStorage.getItem('token')
-                         }
-                        }).then(function (response) {
-                            console.log(response);
-                            console.log(response.data.msg);
-                            console.log(localStorage.getItem('token'));
-                            localStorage.setItem('user',JSON.stringify(response.data.msg))
-                            const user=localStorage.getItem('user')
-                            console.log('User from local storage',JSON.parse(user));
-                           
-                          
-                         })
-                        .catch(function (error) {
-                            console.log(error);
-                            
-                        });
+        axios.post('http://localhost:3000/user_token', 
+                    data).then(response => {
+                                                console.log(response);
+                                                console.log(response.data.jwt);
+                                                console.log(response.status)
+                                                if (response.status == 201)
+                                                {
+                                                    localStorage.setItem('token',response.data.jwt)
+                                                    this.setState({ redirect: true}); 
+                                                    axios.get('http://localhost:3000/auth', 
+                                                            {   headers:
+                                                                {
+                                                                    'Content-Type': 'application/json',
+                                                                    'Authorization':"Bearer "+localStorage.getItem('token')
+                                                                }
+                                                            }).then(function (response) 
+                                                                {
+                                                                    console.log(response);
+                                                                    console.log(response.data.msg);
+                                                                    console.log(localStorage.getItem('token'));
+                                                                    localStorage.setItem('user',JSON.stringify(response.data.msg))
+                                                                    const user=localStorage.getItem('user')
+                                                                    console.log('User from local storage',JSON.parse(user));              
+                                                            }).catch(function (error) 
+                                                                    {
+                                                                        console.log(error);   
+                                                                    });
+                                                }
+                                            })
+                        .catch(error => 
+                            {
+                                console.log(error);
+                                this.setState({ errors: "Incorrect Email or Password"});
+                            });
+    }
 
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-            
-          });
+
+    handleSocialLogin = (user) => {
+                                    console.log(user)
+                                    // const userpass = randomString({length: 8});
+                                    let body = {"user":{
+                                                            name:user.profile.name,
+                                                            email:user.profile.email,
+                                                            password: '1234',
+                                                            image:user.profile.profilePicURL
+                                                        }
+                                                };
+                                    axios.post('http://localhost:3000/users', 
+                                            body).then(response => {
+                                                        let body = { "auth":{
+                                                                                // name:user.profile.name,
+                                                                                email:user.profile.email,
+                                                                                password: '1234',
+                                                                            }
+                                                                   }
+                                                        axios.post('http://localhost:3000/user_token', 
+                                                            body).then(response => {
+                                                                                        console.log(response);
+                                                                                        console.log(response.data.jwt);
+                                                                                        console.log(response.status)
+                                                                                        if (response.status == 201)
+                                                                                        {
+                                                                                            localStorage.setItem('token',response.data.jwt)
+                                                                                            // this.setState({ redirect: true}); 
+                                                                                            axios.get('http://localhost:3000/auth', 
+                                                                                                {   headers:{
+                                                                                                                'Content-Type': 'application/json',
+                                                                                                                'Authorization':"Bearer "+localStorage.getItem('token')
+                                                                                                             }
+                                                                                                }).then(response => {
+                                                                                                                        console.log(response);
+                                                                                                                        console.log(response.data.msg);
+                                                                                                                        console.log(localStorage.getItem('token'));
+                                                                                                                        localStorage.setItem('user',JSON.stringify(response.data.msg))
+                                                                                                                        const user=localStorage.getItem('user')
+                                                                                                                        console.log('User from local storage',JSON.parse(user));                                                                                                
+                                                                                                                        this.setState({ redirect: true});
+                                                                                                                    })
+                                                                                                  .catch(function (error) 
+                                                                                                    {
+                                                                                                        console.log(error);
+                                                                                                    }); 
+                                                                                        }
+                                                                                    })
+                                                                .catch(function (error) 
+                                                                    {
+                                                                        console.log(error);  
+                                                                    });   
+                                                }).catch(function (error) 
+                                                    {
+                                                        console.log(error);
+                                                    });
+                                    }
+
+    handleSocialLoginFailure(error)
+    {
+        console.log(error)
     }
 
 
@@ -88,7 +153,15 @@ class Login extends Component {
                             
                                 Yalla Notlob
                         </Header>           
-                        
+                        <label>
+                            { this.state.errors !=''?
+                            <Message
+                            error
+                            header=''
+                            content={this.state.errors}
+                            />
+                            :''}
+                        </label>
                         <Form size='large'>
                             <Segment stacked>  
                                     <Form.Input
@@ -130,8 +203,23 @@ class Login extends Component {
                 </Grid>
                     <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle' >
                     <Grid.Column style={{ maxWidth: 300 }}> 
+
+                    <div>
+                        <SocialButton
+                            provider='google'
+                            appId='753789024014-5sk71rbmsl74c084v0rvhk7vlo43eolb.apps.googleusercontent.com'
+                            onLoginSuccess={this.handleSocialLogin}
+                            onLoginFailure={this.handleSocialLoginFailure}
+                        >
+                        <GoogleLoginButton />
+                        </SocialButton>
+                    </div>
+
+
+
+
                     <FacebookLoginButton onClick={() => alert('Hello')} />
-                    <GoogleLoginButton onClick={() => alert('Hello')} />
+                    {/* <GoogleLoginButton onClick={() => alert('Hello')} /> */}
                     </Grid.Column>
                 </Grid>
             </div>
