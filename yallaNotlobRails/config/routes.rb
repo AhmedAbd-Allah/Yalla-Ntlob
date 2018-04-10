8bed561db756bd58808864b1eb983ee3ffc78479
SarahHeshamNadrin <sarah.nadrin@gmail.com>
2018-04-10 18:36:22 +0200

    update status of order invitation

--- yallaNotlobRails/config/routes.rb
+++ yallaNotlobRails/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   
 
-  resources :order_invitations do
+  resources :order_invitations ,except: [:update] do 
   collection do
     put '/update' => 'order_invitations#updateStatus'
   end
